35a0cba1fdedc7bd2c70f19bb65a42272ee6e51c
Toni Mäki <toni.maki@vtt.fi>
2016-02-22 15:28:35 +0200

    MediaConfiguration

---
+++ webrtcpeer-android/src/main/java/fi/vtt/nubomedia/webrtcpeerandroid/NBMMediaConfiguration.java
@@ -0,0 +1,81 @@
+package fi.vtt.nubomedia.webrtcpeerandroid;
+
+import android.graphics.ImageFormat;
+
+import org.webrtc.VideoRenderer;
+
+/**
+ *
+ */
+public class NBMMediaConfiguration {
+
+
+    public enum NBMRendererType {
+        NATIVE, OPENGLES
+    }
+
+    public enum NBMAudioCodec {
+        OPUS, ISAC
+    }
+
+    public enum NBMVideoCodec {
+        VP8, VP9, H264
+    }
+
+
+    public static class NBMVideoFormat {
+        public final int heigth;
+        public final int width;
+        public final int imageFormat;
+        public final double frameRate;
+
+        public NBMVideoFormat(int width, int heigth, int imageFormat, double frameRate) {
+            this.width = width;
+            this.heigth = heigth;
+
+            this.imageFormat = imageFormat;
+            this.frameRate = frameRate;
+        }
+    }
+
+    public enum NBMCameraPosition {
+        ANY, BACK, FRONT
+    }
+
+
+    private NBMRendererType rendererType;
+    private NBMAudioCodec audioCodec;
+    private int audioBandwidth;
+
+    private NBMVideoCodec videoCodec;
+    private int videoBandwidth;
+
+    NBMVideoFormat receiverVideoFormat;
+    NBMCameraPosition cameraPosition;
+
+    public NBMMediaConfiguration() {
+        rendererType = NBMRendererType.NATIVE
+        audioCodec = NBMAudioCodec.OPUS;
+        audioBandwidth = 0;
+
+        videoCodec = NBMVideoCodec.VP8;
+        videoBandwidth = 0;
+
+        receiverVideoFormat = new NBMVideoFormat(640,480, ImageFormat.NV21 , 30);
+        cameraPosition = NBMCameraPosition.FRONT;
+
+    }
+
+    public NBMMediaConfiguration(NBMRendererType rendererType, NBMAudioCodec audioCodec,
+                                 int audioBandwidth, NBMVideoCodec videoCodec,
+                                 int videoBandwidth, NBMVideoFormat receiverVideoFormat,
+                                 NBMCameraPosition cameraPosition) {
+        this.rendererType = rendererType;
+        this.audioCodec = audioCodec;
+        this.audioBandwidth = audioBandwidth;
+        this.videoCodec = videoCodec;
+        this.videoBandwidth = videoBandwidth;
+        this.receiverVideoFormat = receiverVideoFormat;
+        this.cameraPosition = cameraPosition;
+    }
+}
---
+++ webrtcpeer-android/src/main/java/fi/vtt/nubomedia/webrtcpeerandroid/NBMWebRTCPeer.java
@@ -0,0 +1,7 @@
+package fi.vtt.nubomedia.webrtcpeerandroid;
+
+/**
+ * Created by tmtoni on 22.2.2016.
+ */
+public class NBMWebRTCPeer {
+}
