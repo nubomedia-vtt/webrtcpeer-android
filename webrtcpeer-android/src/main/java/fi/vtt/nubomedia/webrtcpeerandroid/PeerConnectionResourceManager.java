c06b96cd69e8eff0e53a581e05aea455a7eaaa5f
Toni Mäki <toni.maki@vtt.fi>
2016-02-26 10:38:26 +0200

    - Included the Google copyright - Changed the visibilities - Removed PeerConnectionClient.java

--- webrtcpeer-android/src/main/java/fi/vtt/nubomedia/webrtcpeerandroid/PeerConnectionResourceManager.java
+++ webrtcpeer-android/src/main/java/fi/vtt/nubomedia/webrtcpeerandroid/PeerConnectionResourceManager.java
@@ -21,9 +21,41 @@ import java.util.LinkedList;
 import fi.vtt.nubomedia.utilitiesandroid.LooperExecutor;
 
 /**
- * Created by tmtoni on 24.2.2016.
+ * The class implements the management of PeerConnection instances.
+ *
+ * The implementation is based on PeerConnectionClient.java of package org.appspot.apprtc
+ * (please see the copyright notice below)
  */
-public class PeerConnectionResourceManager {
+
+/*
+ * libjingle
+ * Copyright 2014 Google Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+final class PeerConnectionResourceManager {
 
     private static final String TAG = "PCResourceManager";
 
@@ -50,7 +82,7 @@ public class PeerConnectionResourceManager {
 
 
 
-    public PeerConnectionResourceManager(NBMWebRTCPeer.NBMPeerConnectionParameters peerConnectionParameters,
+    PeerConnectionResourceManager(NBMWebRTCPeer.NBMPeerConnectionParameters peerConnectionParameters,
                                          LooperExecutor executor, PeerConnectionFactory factory) {
 
         this.peerConnectionParameters = peerConnectionParameters;
@@ -81,7 +113,7 @@ public class PeerConnectionResourceManager {
      *
      * @param signalingParameters
      */
-    public NBMPeerConnection createPeerConnection(NBMWebRTCPeer.SignalingParameters signalingParameters,
+    NBMPeerConnection createPeerConnection(NBMWebRTCPeer.SignalingParameters signalingParameters,
                                      MediaConstraints pcConstraints,
                                      String connectionId) {
         if (peerConnectionParameters == null) {
